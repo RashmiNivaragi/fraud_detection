3c16567fc0151632a9dbc1b1565d3d2ee76b81ff
RashmiNivaragi <rashmi.nivaragi@gmail.com>
2024-01-28 19:27:11 +0530

    add data processing file

---
+++ data_processing.py
@@ -0,0 +1,13 @@
+print("adding all data processing code")
+
+a = 5
+b = 3
+
+sum = a+ b
+print("Result Addition:",sum)
+
+sub = a- b 
+print("Result Substraction:",sub)
+
+mul = a * b
+print("Result Multiplication:",mul)
